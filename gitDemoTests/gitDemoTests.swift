b45e9a7e9653fde57f256b937e1dcb7932c6a2f9
wangwenwen <wangwenwen@quzubuluo.com>
2018-11-09 22:59:25 +0800

    提交

---
+++ gitDemoTests/gitDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  gitDemoTests.swift
+//  gitDemoTests
+//
+//  Created by 王文文 on 2018/11/9.
+//  Copyright © 2018年 wangwen. All rights reserved.
+//
+
+import XCTest
+@testable import gitDemo
+
+class gitDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
